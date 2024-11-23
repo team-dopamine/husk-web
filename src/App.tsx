517c27ac6d1658b93f7015ae2825f4273140406a
dev-joohee <dev.joohee0916@gmail.com>
2024-11-23 21:17:17 +0900

    refactor: 컴포넌트 구조 변경

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import Header from './components/Header';
+import Header from './components/header';
 
 const App: React.FC = () => {
   return (
--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { HeaderWrapper, LogoWrapper, LogoImg, Logo, LoginButton } from '../styles/Header-styled';
+import { HeaderWrapper, LogoWrapper, LogoImg, Logo, LoginButton } from './index.style';
 
 const Header: React.FC = () => {
   return (

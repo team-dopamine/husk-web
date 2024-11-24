b537616e02d8e722fb60f0b2bbb5f7144de2eaae
Jinwoo Lee <binaryWooo@gmail.com>
2024-11-25 03:26:30 +0900

    style: footer css 설정

--- src/components/footer/index.style.tsx
+++ src/components/footer/index.style.tsx
@@ -1,6 +1,26 @@
 import { Button } from 'antd';
 import styled from 'styled-components';
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  width: 100%;
+  padding: 20px 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 100px;
+`;
 
-export const Link = styled(Button)``;
+export const Link = styled(Button)`
+  border-radius: 0;
+  padding: 0;
+  height: auto;
+  border: none;
+  background: transparent;
+  position: relative;
+
+  &:hover,
+  &:focus {
+    background: transparent;
+    border: none;
+  }
+`;

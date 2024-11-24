a49dab478ff2a83752e6b966704a47bf8ff35b60
Jinwoo Lee <binaryWooo@gmail.com>
2024-11-25 03:26:57 +0900

    fix: footer 하위 링크 구분을 위한 span 태그 추가

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -5,8 +5,11 @@ const Footer = () => {
     <>
       <Container>
         <Link type="text">© Team Dopamine.</Link>
+        <span>|</span>
         <Link type="text">서비스 피드백</Link>
+        <span>|</span>
         <Link type="text">이용약관</Link>
+        <span>|</span>
         <Link type="text">개인정보처리방침</Link>
       </Container>
     </>

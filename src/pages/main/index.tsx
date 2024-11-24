2044b30ff6bf17e3d83fc7981cd2f9d1aad5a4c1
Jinwoo Lee <binaryWooo@gmail.com>
2024-11-25 03:31:26 +0900

    feat: main page 구조 작성

---
+++ src/pages/main/index.tsx
@@ -0,0 +1,14 @@
+import Header from '../../components/header';
+import Footer from '../../components/footer';
+
+const Main = () => {
+  return (
+    <>
+      <Header />
+      {/* Contents */}
+      <Footer />
+    </>
+  );
+};
+
+export default Main;

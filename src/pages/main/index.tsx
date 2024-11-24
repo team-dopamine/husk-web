233669e533f7288eb20fdc8f4991770b5cf8b388
Jinwoo Lee <binaryWooo@gmail.com>
2024-11-25 03:40:44 +0900

    style: main page 레이아웃 추가 및  스타일 설정

--- src/pages/main/index.tsx
+++ src/pages/main/index.tsx
@@ -1,12 +1,15 @@
 import Header from '../../components/header';
 import Footer from '../../components/footer';
+import { Content, Layout } from './index.style';
 
 const Main = () => {
   return (
     <>
-      <Header />
-      {/* Contents */}
-      <Footer />
+      <Layout>
+        <Header />
+        <Content>Empty</Content>
+        <Footer />
+      </Layout>
     </>
   );
 };

4917e9077547b36ff7cf6f79ef01caa336cccf6e
dev-joohee <dev.joohee0916@gmail.com>
2025-01-01 15:33:53 +0900

    refactor: 절대경로로 수정

--- src/pages/main/index.tsx
+++ src/pages/main/index.tsx
@@ -1,5 +1,5 @@
-import Header from '../../components/header';
-import Footer from '../../components/footer';
+import Header from '@components/header';
+import Footer from '@components/footer';
 import { Layout } from './index.style';
 import Content from './content';
 

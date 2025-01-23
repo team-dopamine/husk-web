9198902d5dcab362488eaf7b284c731f8f34e193
dev-joohee <dev.joohee0916@gmail.com>
2025-01-23 18:51:46 +0900

    feat: 서비스 사용자 약관 조회 api 연동

--- src/pages/sign-up/read-terms/read-terms-content/index.tsx
+++ src/pages/sign-up/read-terms/read-terms-content/index.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import {
   Label,
   HiddenCheckbox,
@@ -14,9 +14,26 @@ import {
   AgreementCheckboxWrapper,
   AgreementText,
 } from './index.style';
+import getTermsOfService from 'api/read-terms';
 
 const ReadTermsContent = () => {
   const [isChecked, setIsChecked] = useState(false);
+  const [termsContent, setTermsContent] = useState('약관을 불러오는 중입니다...');
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    const fetchTerms = async () => {
+      try {
+        const terms = await getTermsOfService();
+        console.log('약관 내용:', terms);
+        setTermsContent(terms);
+      } catch (err) {
+        setError('약관을 불러오는 중 오류가 발생했습니다.');
+      }
+    };
+
+    fetchTerms();
+  }, []);
 
   return (
     <Container>
@@ -24,7 +41,7 @@ const ReadTermsContent = () => {
         <Title>Sign Up</Title>
         <TermsWrapper>
           <Label>이용약관</Label>
-          <TermsTextarea readOnly value="약관 내용" />
+          <TermsTextarea readOnly value={error || termsContent} />
           <AgreementCheckboxWrapper>
             <AgreementText>상기 내용에 동의합니다.</AgreementText>
             <HiddenCheckbox type="checkbox" id="customCheckbox" checked={isChecked} onChange={(e) => setIsChecked(e.target.checked)} />
@@ -32,10 +49,10 @@ const ReadTermsContent = () => {
           </AgreementCheckboxWrapper>
         </TermsWrapper>
         <ButtonGroup>
-          <PreviousButton type="primary" href="/sign-up">
+          <PreviousButton type="primary" href="/sign-in">
             Previous
           </PreviousButton>
-          <NextButton type="primary" disabled={!isChecked}>
+          <NextButton type="primary" disabled={!isChecked} href="/sign-up">
             Next
           </NextButton>
         </ButtonGroup>

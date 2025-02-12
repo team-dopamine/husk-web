0039eb4775de2993e0a1ce26167ab2c7fa13be6b
dev-joohee <dev.joohee0916@gmail.com>
2025-02-13 00:12:11 +0900

    feat: 로그인 api 연동

--- src/pages/sign-in/sign-in-email/content/index.tsx
+++ src/pages/sign-in/sign-in-email/content/index.tsx
@@ -1,27 +1,49 @@
-import { useState } from 'react';
+import { useState, useContext } from 'react';
+import { useNavigate } from 'react-router-dom';
+import postSignIn from 'api/sign-in';
+import { AuthContext } from 'api/context/auth-context';
 import { Container, ContentWrapper, Title, InputField, LoginButton, Label } from '@pages/sign-in/sign-in-email/content/index.style';
+import Header from '@components/header';
 
 const SigninContent: React.FC = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [loading, setLoading] = useState(false);
+  const { accessToken, login, isLoggedIn } = useContext(AuthContext)!;
+  const navigate = useNavigate();
 
-  const handleLogin = () => {
-    console.log('Email:', email, 'Password:', password);
+  const handleLogin = async () => {
+    try {
+      setLoading(true);
+      const response = await postSignIn({ email, password });
+      if (response.jwtTokenDto.accessToken) {
+        login(response.jwtTokenDto.accessToken);
+        navigate('/');
+      }
+    } catch (error) {
+      alert(error instanceof Error ? error.message : '로그인 중 오류가 발생했습니다.');
+    } finally {
+      setLoading(false);
+    }
   };
 
   return (
-    <Container>
-      <ContentWrapper>
-        <Title>Sign In</Title>
-        <Label style={{ alignSelf: 'flex-start' }}>Email</Label>
-        <InputField type="email" placeholder="Enter your email" value={email} onChange={(e) => setEmail(e.target.value)} required />
+    <>
+      <Container>
+        <ContentWrapper>
+          <Title>Sign In</Title>
+          <Label style={{ alignSelf: 'flex-start' }}>Email</Label>
+          <InputField type="email" placeholder="Enter your email" value={email} onChange={(e) => setEmail(e.target.value)} required />
 
-        <Label style={{ alignSelf: 'flex-start' }}>Password</Label>
-        <InputField type="password" placeholder="Enter your password" value={password} onChange={(e) => setPassword(e.target.value)} required />
+          <Label style={{ alignSelf: 'flex-start' }}>Password</Label>
+          <InputField type="password" placeholder="Enter your password" value={password} onChange={(e) => setPassword(e.target.value)} required />
 
-        <LoginButton onClick={handleLogin}>Login</LoginButton>
-      </ContentWrapper>
-    </Container>
+          <LoginButton onClick={handleLogin} disabled={loading}>
+            {loading ? 'Logging in...' : 'Login'}
+          </LoginButton>
+        </ContentWrapper>
+      </Container>
+    </>
   );
 };
 

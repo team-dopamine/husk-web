ad4c862a26f53a678c81e3568a2f8a2f848ff05b
dev-joohee <dev.joohee0916@gmail.com>
2025-02-13 00:13:37 +0900

    feat: auth provider 적용

--- src/App.tsx
+++ src/App.tsx
@@ -8,21 +8,24 @@ import ReadTerms from '@pages/sign-up/content/read-terms';
 import Password from '@pages/sign-up/content/password';
 import Frame from '@components/frame';
 import AuthSignIn from '@pages/sign-in/sign-in-email';
+import { AuthProvider } from 'api/context/auth-provider';
 
 const App: React.FC = () => {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Frame />}>
-          <Route index element={<Main />} />
-          <Route path="sign-in" element={<SignIn />} />
-          <Route path="auth-sign-in" element={<AuthSignIn />} />
-          <Route path="sign-up" element={<SignUp />} />
-          <Route path="password" element={<Password />} />
-          <Route path="read-terms" element={<ReadTerms />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <AuthProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Frame />}>
+            <Route index element={<Main />} />
+            <Route path="sign-in" element={<SignIn />} />
+            <Route path="auth-sign-in" element={<AuthSignIn />} />
+            <Route path="sign-up" element={<SignUp />} />
+            <Route path="password" element={<Password />} />
+            <Route path="read-terms" element={<ReadTerms />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </AuthProvider>
   );
 };
 

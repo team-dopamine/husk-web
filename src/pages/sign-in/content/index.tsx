b60fe762bb82d69c41217af0942a32c431e8d0dd
dev-joohee <dev.joohee0916@gmail.com>
2025-02-14 18:36:20 +0900

    feat: 깃허브 로그인 api 호출

--- src/pages/sign-in/content/index.tsx
+++ src/pages/sign-in/content/index.tsx
@@ -2,6 +2,8 @@ import { useGoogleLogin } from '@react-oauth/google';
 import { useNavigate } from 'react-router-dom';
 import { ButtonGroup, Container, ContentWrapper, OAuthButton, SignUpLink, SignUpText, SignUpWrapper, Title } from './index.style';
 
+const GITHUB_CLIENT_ID = process.env.REACT_APP_GITHUB_CLIENT_ID;
+const GITHUB_REDIRECT_URI = process.env.REACT_APP_GITHUB_REDIRECT_URI;
 const Content = () => {
   const navigate = useNavigate();
 
@@ -11,11 +13,21 @@ const Content = () => {
     redirect_uri: process.env.REACT_APP_REDIRECT_URI || '',
   });
 
+  const githubLogin = () => {
+    if (!GITHUB_CLIENT_ID || !GITHUB_REDIRECT_URI) {
+      console.error('GitHub OAuth 설정이 올바르지 않습니다. .env 파일을 확인하세요.');
+      return;
+    }
+    const githubAuthUrl = `https://github.com/login/oauth/authorize?client_id=${GITHUB_CLIENT_ID}&redirect_uri=${GITHUB_REDIRECT_URI}&scope=user:email`;
+    window.location.href = githubAuthUrl;
+  };
   const handleOAuthSignIn = (provider: string) => {
     if (provider === 'email') {
       navigate('/auth-sign-in');
     } else if (provider === 'google') {
       googleLogin();
+    } else if (provider === 'github') {
+      githubLogin();
     }
   };
 
---
+++ src/pages/sign-in/sign-in-github/index.tsx
@@ -0,0 +1,40 @@
+import { useSearchParams, useNavigate } from 'react-router-dom';
+import { useContext, useEffect, useState } from 'react';
+import getGithub from 'api/sign-in/github';
+import { AuthContext } from 'api/context/auth-context';
+
+const Github = () => {
+  const [searchParams] = useSearchParams();
+  const [githubStatus, setGithubStatus] = useState<number | null>(null);
+  const { login } = useContext(AuthContext)!;
+  const navigate = useNavigate();
+
+  const code = searchParams.get('code');
+  console.log('Github OAuth Code:', code);
+
+  useEffect(() => {
+    const fetchGithubAuth = async () => {
+      try {
+        const response = await getGithub(code);
+        const status = response.statusCodeValue;
+        setGithubStatus(status);
+        if (status == 200) {
+          if (response.body.jwtTokenDto.accessToken) {
+            login(response.body.jwtTokenDto.accessToken);
+            navigate('/');
+          }
+        } else if (status == 400) {
+          navigate('/sign-in');
+        }
+      } catch (err) {
+        console.error('Github 로그인 오류:', err);
+      }
+    };
+
+    fetchGithubAuth();
+  }, [code, navigate]);
+
+  return <p>Github 로그인 처리 중...</p>;
+};
+
+export default Github;

9bf600f9bccc33144142b1cd94d7ea8d14c1e819
soooheeee <tgb0626@naver.com>
2025-03-24 01:02:10 +0900

    refactor: dashboard-sidebar 경로 업데이트

--- src/components/dashboard/layout/dashboard-frame/index.tsx
+++ src/components/dashboard/layout/dashboard-frame/index.tsx
@@ -1,6 +1,6 @@
 import { Outlet } from 'react-router-dom';
 import { DashboardLayout } from './index.style';
-import Sidebar from '@components/sidebar';
+import Sidebar from '@components/dashboard/layout/dashboard-sidebar';
 import DashboardHeader from '@components/dashboard/layout/dashboard-header';
 
 const DashboardFrame: React.FC = () => {

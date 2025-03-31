b0a51caf2957f1b5aceeb6a513358175e10a5cea
soooheeee <tgb0626@naver.com>
2025-03-31 10:42:52 +0900

    feat: 키체인 복호화 API 함수 및 관련 타입 정의 추가

---
+++ src/api/keychains/keychain-decrypt/index.tsx
@@ -0,0 +1,33 @@
+import api from 'api/axios';
+import { AxiosError } from 'axios';
+
+interface KeychainResponse {
+  seletedData: number;
+  name: string;
+  content: string;
+}
+
+interface ErrorResponse {
+  message?: string;
+}
+
+const getKeychainDecrypt = async (seletedData: any): Promise<KeychainResponse> => {
+  try {
+    const response = await api.get(`keychains/${seletedData}/decrypt`, {
+      headers: { 'Content-Type': 'application/json' },
+    });
+    return response.data;
+  } catch (error: unknown) {
+    if (error instanceof AxiosError) {
+      if (error.response) {
+        const errorMessage = (error.response.data as ErrorResponse)?.message || '저장에 실패했습니다';
+        throw new Error(errorMessage);
+      } else if (error.request) {
+        throw new Error('네트워크 문제 또는 서버가 응답하지 않습니다.');
+      }
+    }
+    throw new Error('알 수 없는 오류가 발생했습니다.');
+  }
+};
+
+export default getKeychainDecrypt;

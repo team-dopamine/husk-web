7dc253bd12f3b742c0adc9885b6d8c66e0099e7d
Kim joohee <dev.joohee0916@gmail.com>
2025-04-09 15:41:28 +0900

    feat: 커넥션 접속 요청 api 연동 및 임시 페이지 연결

--- src/pages/ssh-connection/index.tsx
+++ src/pages/ssh-connection/index.tsx
@@ -2,9 +2,12 @@ import { useEffect, useState } from 'react';
 import getSshConnections, { SshConnection } from 'api/ssh-connections/ssh-read';
 import { SshConnectionContainer } from './index.style';
 import KeychainCard from '@components/dashboard/cards/keychain-card';
+import postSshSession from 'api/ssh-connections/ssh-session';
+import { useNavigate } from 'react-router-dom';
 
 const SshConnectionPage = () => {
   const [sshConnections, setSshConnections] = useState<SshConnection[]>([]);
+  const navigate = useNavigate();
 
   useEffect(() => {
     const fetchData = async () => {
@@ -19,10 +22,19 @@ const SshConnectionPage = () => {
     fetchData();
   }, []);
 
+  const handleSshSession = async (id: number) => {
+    try {
+      await postSshSession(id);
+      navigate('/dashboard/test-page');
+    } catch (error) {
+      console.error('SSH 접속 요청 실패:', error);
+    }
+  };
+
   return (
     <SshConnectionContainer>
       {sshConnections.map(({ id, name, host, port }) => (
-        <KeychainCard key={id} id={id} title={name} label={`${host}:${port}`} />
+        <KeychainCard key={id} id={id} title={name} label={`${host}:${port}`} onClick={() => handleSshSession(id)} />
       ))}
     </SshConnectionContainer>
   );

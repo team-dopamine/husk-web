f13a56e8c721f56120f2aa3cd3d8466f4d3dc7e0
soooheeee <tgb0626@naver.com>
2025-05-09 13:40:51 +0900

    feat: ssh 커넥션 api 호출

--- src/components/dashboard/button/saveButton/index.tsx
+++ src/components/dashboard/button/saveButton/index.tsx
@@ -1,13 +1,15 @@
 import postKeychain from 'api/keychains/keychain-register';
 import { Container, ActionButton } from './index.style';
 import { getStoredToken } from 'api/context/auth-util';
+import postSshConnection from 'api/ssh-connections/ssh-save/ssh-connection-register';
 
 type SaveButtonGroupProps = {
   inputValues: string[];
+  currentPage: string;
   onClose: () => void;
 };
 
-const SaveButton: React.FC<SaveButtonGroupProps> = ({ inputValues, onClose }) => {
+const SaveButton: React.FC<SaveButtonGroupProps> = ({ inputValues, onClose, currentPage }) => {
   const handleSave = async () => {
     const accessToken = getStoredToken();
 
@@ -15,25 +17,49 @@ const SaveButton: React.FC<SaveButtonGroupProps> = ({ inputValues, onClose }) =>
       alert('로그인이 필요합니다.');
       return;
     }
+    if (currentPage == 'keychain') {
+      try {
+        const trimmedValues = inputValues.map((value) => value.trim());
 
-    try {
-      const trimmedValues = inputValues.map((value) => value.trim());
+        if (trimmedValues.some((value) => value === '')) {
+          alert('모든 필드를 입력하세요.');
+          return;
+        }
 
-      if (trimmedValues.some((value) => value === '')) {
-        alert('모든 필드를 입력하세요.');
-        return;
+        await postKeychain({
+          name: inputValues[0],
+          content: inputValues[1],
+          accessToken,
+        });
+
+        onClose();
+      } catch (error) {
+        console.error('Error posting data:', error);
+        alert(error instanceof Error ? error.message : '데이터 저장에 실패했습니다.');
       }
+    } else if (currentPage == 'ssh-connection') {
+      try {
+        const trimmedValues = inputValues.map((value) => value.trim());
 
-      await postKeychain({
-        name: inputValues[0],
-        content: inputValues[1],
-        accessToken,
-      });
+        if (trimmedValues.some((value) => value === '')) {
+          alert('모든 필드를 입력하세요.');
+          return;
+        }
 
-      onClose();
-    } catch (error) {
-      console.error('Error posting data:', error);
-      alert(error instanceof Error ? error.message : '데이터 저장에 실패했습니다.');
+        await postSshConnection({
+          name: inputValues[0],
+          host: inputValues[1],
+          username: inputValues[2],
+          port: inputValues[3],
+          keyChainName: inputValues[4],
+          accessToken,
+        });
+
+        onClose();
+      } catch (error) {
+        console.error('Error posting data:', error);
+        alert(error instanceof Error ? error.message : '데이터 저장에 실패했습니다.');
+      }
     }
   };
 

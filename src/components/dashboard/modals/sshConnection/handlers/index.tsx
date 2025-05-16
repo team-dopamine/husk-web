a68315cdffa685814bdb3f24b9fb39cee0124c5e
soooheeee <tgb0626@naver.com>
2025-05-16 15:14:01 +0900

    feat: ssh 커넥션 수정 api 요청 및 관련 함수 구현

--- src/components/dashboard/modals/sshConnection/handlers/index.tsx
+++ src/components/dashboard/modals/sshConnection/handlers/index.tsx
@@ -1,4 +1,5 @@
 import deleteSshConnection from 'api/ssh-connections/ssh-delete';
+import patchSshConnectionUpdate from 'api/ssh-connections/ssh-update';
 
 /**커넥션 삭제 api 호출 핸들러 */
 export const handleSshDelete = async (id: number, onClose: () => void, onSuccess?: () => void) => {
@@ -16,5 +17,24 @@ export const handleSshDelete = async (id: number, onClose: () => void, onSuccess
   } catch (error) {
     console.error('삭제 오류:', error);
     alert(error instanceof Error ? error.message : '삭제 실패');
+  }
+};
+
+export const handleSshUpdate = async (id: number, inputValues: string[], onClose: () => void, onSuccess?: () => void) => {
+  if (!id) {
+    alert('수정할 키체인을 찾을 수 없습니다.');
+    return;
+  }
+
+  const [name, host, port, username, keyChainName] = inputValues.map((v) => v.trim());
+  const data = { name, host, port, username, keyChainName, id };
+
+  try {
+    await patchSshConnectionUpdate(data);
+    onClose();
+    onSuccess?.();
+  } catch (error) {
+    console.error(error);
+    alert(error instanceof Error ? error.message : '업데이트 실패');
   }
 };

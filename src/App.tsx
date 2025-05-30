27998fd2a428e77a010bbfb14ad563abe0f70b64
Kim joohee <dev.joohee0916@gmail.com>
2025-05-30 15:45:11 +0900

    feat: globalstyle 적용

--- src/App.tsx
+++ src/App.tsx
@@ -18,36 +18,40 @@ import Dashboard from '@pages/dashboard';
 import KeychainPage from '@pages/keychain';
 import SshConnectionPage from '@pages/ssh-connection';
 import TerminalPage from '@pages/terminal';
+import GlobalStyle from '@styles/global/GlobalStyle';
 
 const clientId = process.env.REACT_APP_GOOGLE_CLIENT_ID || '';
 
 const App: React.FC = () => {
   return (
-    <GoogleOAuthProvider clientId={clientId}>
-      <AuthProvider>
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<Frame />}>
-              <Route index element={<Main />} />
-              <Route path="signin" element={<SignIn />} />
-              <Route path="auth/signin" element={<AuthSignIn />} />
-              <Route path="signup" element={<SignUp />} />
-              <Route path="terms" element={<ReadTerms />} />
-              <Route path="password" element={<Password />} />
-              <Route path="password/reset" element={<ResetPassword />} />
-              <Route path="password/confirm" element={<TempUpdatePasswordConfirm />} />
-              <Route path="password/setting" element={<TempUpdatePasswordSetting />} />
-            </Route>
-            <Route path="/dashboard" element={<DashboardFrame />}>
-              <Route index element={<Dashboard />} />
-              <Route path="keychains" element={<KeychainPage />} />
-              <Route path="connections" element={<SshConnectionPage />} />
-            </Route>
-            <Route path="terminal" element={<TerminalPage />} />
-          </Routes>
-        </BrowserRouter>
-      </AuthProvider>
-    </GoogleOAuthProvider>
+    <>
+      <GlobalStyle />
+      <GoogleOAuthProvider clientId={clientId}>
+        <AuthProvider>
+          <BrowserRouter>
+            <Routes>
+              <Route path="/" element={<Frame />}>
+                <Route index element={<Main />} />
+                <Route path="signin" element={<SignIn />} />
+                <Route path="auth/signin" element={<AuthSignIn />} />
+                <Route path="signup" element={<SignUp />} />
+                <Route path="terms" element={<ReadTerms />} />
+                <Route path="password" element={<Password />} />
+                <Route path="password/reset" element={<ResetPassword />} />
+                <Route path="password/confirm" element={<TempUpdatePasswordConfirm />} />
+                <Route path="password/setting" element={<TempUpdatePasswordSetting />} />
+              </Route>
+              <Route path="/dashboard" element={<DashboardFrame />}>
+                <Route index element={<Dashboard />} />
+                <Route path="keychains" element={<KeychainPage />} />
+                <Route path="connections" element={<SshConnectionPage />} />
+              </Route>
+              <Route path="terminal" element={<TerminalPage />} />
+            </Routes>
+          </BrowserRouter>
+        </AuthProvider>
+      </GoogleOAuthProvider>
+    </>
   );
 };
 
---
+++ src/styles/global/GlobalStyle.tsx
@@ -0,0 +1,10 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  :root {
+    --main-color: #131E27;
+    --blue-color: #377276;
+    --skyblue-color: #A2C4CD;
+  }
+`;
+export default GlobalStyle;

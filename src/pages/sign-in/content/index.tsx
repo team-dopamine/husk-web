f0ca0637a4dc59b6c43c7460a3fb8148477de2b1
Kim joohee <dev.joohee0916@gmail.com>
2025-05-30 19:05:58 +0900

    feat: oauth 로그인 사진 추가

--- src/pages/sign-in/content/index.tsx
+++ src/pages/sign-in/content/index.tsx
@@ -1,7 +1,8 @@
 import { useGoogleLogin } from '@react-oauth/google';
 import { useNavigate } from 'react-router-dom';
-import { ButtonGroup, Container, ContentWrapper, OAuthButton, SignUpLink, SignUpText, SignUpWrapper, Title } from './index.style';
-
+import { ButtonGroup, Container, ContentWrapper, OAuthButton, SignUpLink, SignUpText, SignInButton, SignUpWrapper, Title } from './index.style';
+import githubSignIn from 'assets/github-mark-white.png';
+import googleSignInRd from 'assets/google_mark-si.png';
 const GITHUB_CLIENT_ID = process.env.REACT_APP_GITHUB_CLIENT_ID;
 const GITHUB_REDIRECT_URI = process.env.REACT_APP_GITHUB_REDIRECT_URI;
 const Content = () => {
@@ -36,13 +37,16 @@ const Content = () => {
       <ContentWrapper>
         <Title>Sign In</Title>
         <ButtonGroup>
-          <OAuthButton onClick={() => handleOAuthSignIn('google')}>Continue with Google</OAuthButton>
-          <OAuthButton onClick={() => handleOAuthSignIn('github')}>Continue with GitHub</OAuthButton>
-          <OAuthButton onClick={() => handleOAuthSignIn('email')}>Continue with Email</OAuthButton>
+          <SignInButton onClick={() => handleOAuthSignIn('github')}>
+            <img src={githubSignIn} alt="Github Logo" style={{ width: '27px', height: '27px', marginRight: '12px', verticalAlign: 'middle' }} />
+            Sign in with Github
+          </SignInButton>
+          <img src={googleSignInRd} onClick={() => handleOAuthSignIn('google')} alt="Google Logo" style={{ width: '50%', height: '40%', cursor: 'pointer' }} />
+          <SignInButton onClick={() => handleOAuthSignIn('email')}>Sign in with Email</SignInButton>
         </ButtonGroup>
         <SignUpWrapper>
           <SignUpText>
-            계정이 없으신가요? <SignUpLink href="/terms">Sign up</SignUpLink>
+            Don't have an account? <SignUpLink href="/terms">Sign up</SignUpLink>
           </SignUpText>
         </SignUpWrapper>
       </ContentWrapper>

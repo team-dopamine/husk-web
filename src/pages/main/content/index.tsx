b4933a1e3638f313793658b6c5d914464a5a9368
soooheeee <tgb0626@naver.com>
2025-12-03 18:10:04 +0900

    refactor: 하드코딩된 부분 i18n 적용

--- src/pages/main/content/index.tsx
+++ src/pages/main/content/index.tsx
@@ -1,14 +1,15 @@
 import { Banner, Title, Description, ButtonContainer, FunctionButton, StartButton, MainContent, ContentContainer, MainTitle, MainDescription, ContentImage, TextContainer } from './index.style';
+
 import { useNavigate } from 'react-router-dom';
 import { useAuth } from '../../../api/context/use-auth';
 import { useRef } from 'react';
+import { useTranslation } from 'react-i18next';
+
 import SSHGif from 'assets/SSH.gif';
 import KEYCHAINGif from 'assets/KEYCHAIN.gif';
-import { useTranslation } from 'react-i18next';
 
 const Content = () => {
   const { t } = useTranslation();
-
   const navigate = useNavigate();
   const { isLoggedIn } = useAuth();
 
@@ -30,13 +31,16 @@ const Content = () => {
     <>
       <Banner>
         <Title>HUSK: Help Use Shell Kindly</Title>
+
         <Description>{t('home.description')}</Description>
+
         <ButtonContainer>
           <FunctionButton type="primary" onClick={handleScrollClick}>
-            기능 소개
+            {t('home.feature_button')}
           </FunctionButton>
+
           <StartButton type="primary" onClick={handleStartClick}>
-            시작하기
+            {t('home.start_button')}
           </StartButton>
         </ButtonContainer>
       </Banner>
@@ -45,15 +49,15 @@ const Content = () => {
         <ContentContainer>
           <ContentImage src={KEYCHAINGif} ref={scrollRef} />
           <TextContainer>
-            <MainTitle>KeyChain</MainTitle>
-            <MainDescription>SSH 키를 안전하게 저장하고 관리할 수 있습니다. 한눈에 키 목록을 확인하고 손쉽게 추가 또는 수정할 수 있습니다.</MainDescription>
+            <MainTitle>{t('home.keychain.title')}</MainTitle>
+            <MainDescription>{t('home.keychain.description')}</MainDescription>
           </TextContainer>
         </ContentContainer>
         <ContentContainer>
           <ContentImage src={SSHGif} />
           <TextContainer>
-            <MainTitle>SSH Connection</MainTitle>
-            <MainDescription>SSH 연결을 통해 원격 서버에 접속할 수 있습니다.</MainDescription>
+            <MainTitle>{t('home.ssh.title')}</MainTitle>
+            <MainDescription>{t('home.ssh.description')}</MainDescription>
           </TextContainer>
         </ContentContainer>
       </MainContent>
